a04f329baa5e6d2b33e1aa687a3878d9399bed7d
Ajea Smith <ajeasmith@Ajeas-MBP.attlocal.net>
2019-02-19 00:28:43 -0500

    finished layout

--- dropdown-menu/js/main.js
+++ dropdown-menu/js/main.js
@@ -1,13 +1,31 @@
 let togglebtn = document.querySelector(".btn");
+let showNav = false;
 togglebtn.addEventListener("click", () => {
     let getSideMenu = document.querySelector(".side-menu");
-    let getSideList = getSideMenu.firstElementChild;
-    if (!getSideMenu.classList.contains("show")) {
-        getSideMenu.classList.toggle("show");
-        getSideList.classList.toggle("show");
-        console.log("its shown");
-    } else {
-        getSideMenu.classList.remove("show");
-        getSideList.classList.remove("show");
+    let getSideMenuUl = document.querySelector(".side-menu ul");
+    let getSideList = document.querySelectorAll(".side-menu li");
+    let getSpan = document.querySelector(".side-menu li span");
+
+    if(showNav === false){
+        getSideMenuUl.style.visibility = "visible";
+        getSideMenu.style.width = "280px";
+        getSpan.style.opacity = "0.5";
+
+        let liLength = getSideList.length;
+        for (let i = 0; i < liLength.length; i++) {
+           getSideList[i].style.opacity = "1";
+        }
+        showNav = true;
+    }else if (showNav === true){
+        getSideMenu.style.width = "60px";
+        getSpan.style.opacity = "0";
+
+        let liLength = getSideList.length;
+        for (let i = 0; i < liLength.length; i++) {
+           getSideList[i].style.opacity = "0";
+        }
+        getSideMenuUl.style.visibility = "hidden";
+        showNav = false;
     }
+
 })
